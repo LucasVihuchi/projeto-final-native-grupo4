40e77b21861c44d68eb3d4944140a0c2dd62933e
Marcella52 <marcella_hcarvalho@hotmail.com>
2021-12-12 13:38:45 -0300

    App.js com react-native-paper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
 import CadastroUsuario from './pages/CadastroUsuario/cadastroUsuario';
+import theme from './pages/CadastroUsuario/checkStyles';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Página APP</Text>
-      <CadastroUsuario />
-    </SafeAreaView>
+    <PaperProvider theme={theme}>
+      <SafeAreaView>
+        <Text>Página APP</Text>
+        <CadastroUsuario />
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
 
