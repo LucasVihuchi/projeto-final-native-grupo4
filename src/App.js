5c1995fee0223d82db5f09e4912a14a6bd2ba2ac
Lucas Vihuchi <lucasvihuchi@gmail.com>
2021-12-13 12:57:12 -0300

    Ajustes nas rotas e adição de cabeçalho na página de cadastro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import CadastroUsuario from './pages/CadastroUsuario/cadastroUsuario';
 import theme from './pages/CadastroUsuario/checkStyles';
 import ContextoCredenciais from './context/credenciais';
 import Routes from './Routes';
 
-import Header from './components/Header';
-import SubHeader from './components/SubHeader';
 const App = () => {
   return (
     <ContextoCredenciais>
     <PaperProvider theme={theme}>
       <SafeAreaProvider>
-        <SafeAreaView>
-          <CadastroUsuario />
-        </SafeAreaView>
+          <Routes />
       </SafeAreaProvider>
     </PaperProvider>
     </ContextoCredenciais>    
--- src/pages/CadastroUsuario/cadastroUsuario.js
+++ src/pages/CadastroUsuario/cadastroUsuario.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import Formulario from '../../components/FormularioUsuario/formularioUsuario';
+import Header from '../../components/Header';
 
 const CadastroUsuario = () => {
   return (
     <SafeAreaView>
+      <Header />
       <Formulario />
     </SafeAreaView>
   );
